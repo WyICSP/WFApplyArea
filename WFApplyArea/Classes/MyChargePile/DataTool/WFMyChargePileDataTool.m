59ee262b53999ebbee21bc24481ae3ba69570f98
王宇 <wy_wangyu163@163.com>
2020-02-14 12:05:13 +0800

    版本升级维护

--- WFApplyArea/Classes/MyChargePile/DataTool/WFMyChargePileDataTool.m
+++ WFApplyArea/Classes/MyChargePile/DataTool/WFMyChargePileDataTool.m
@@ -83,7 +83,7 @@
 + (void)adminCreditTemplateWithParams:(NSDictionary *)params
                           resultBlock:(void(^)(WFCreditPayModel *models))resultBlock {
     //接口地址
-    NSString *path = [NSString stringWithFormat:@"%@app-partner/adminCreditTemplate/adminDeposit",NEW_HOST_URL];
+    NSString *path = [NSString stringWithFormat:@"%@app-partner/adminCreditTemplate/getAdminPayMethod",NEW_HOST_URL];
     [WKRequest getWithURLString:path parameters:params isShowHud:YES success:^(WKBaseModel *baseModel) {
         if (CODE_ZERO) {
             resultBlock([WFCreditPayModel mj_objectWithKeyValues:baseModel.data]);
@@ -98,7 +98,7 @@
 + (void)addAdminCreditTemplAteadminDepositWithParams:(NSDictionary *)params
                                          resultBlock:(void(^)(WFCheditPayMothedModel *models))resultBlock {
     //接口地址POST
-    NSString *path = [NSString stringWithFormat:@"%@app-partner/adminCreditTemplate/getAdminPayMethod",NEW_HOST_URL];
+    NSString *path = [NSString stringWithFormat:@"%@app-partner/adminCreditTemplate/adminDeposit",NEW_HOST_URL];
     [WKRequest postWithURLString:path parameters:params isJson:YES isShowHud:YES success:^(WKBaseModel *baseModel) {
         if (CODE_ZERO) {
             resultBlock([WFCheditPayMothedModel mj_objectWithKeyValues:baseModel.data]);
