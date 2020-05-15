b9774836ad0d5a2af0c849cf31100545f704ebc3
王宇 <wy_wangyu163@163.com>
2020-05-15 09:26:24 +0800

    版本升级维护

--- Example/Pods/Target Support Files/Pods-WFApplyArea_Example/Pods-WFApplyArea_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-WFApplyArea_Example/Pods-WFApplyArea_Example-frameworks.sh
@@ -162,6 +162,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/IQKeyboardManager/IQKeyboardManager.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJExtension/MJExtension.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
@@ -174,12 +175,14 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/WFKitLogin/WFKitLogin.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WFKitMain/WFKitMain.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WFShopMall/WFShopMall.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/WFStatistics/WFStatistics.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WKFunComponents/WKFunComponents.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WYAutoLayout/WYAutoLayout.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/dsBridge/dsBridge.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/IQKeyboardManager/IQKeyboardManager.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJExtension/MJExtension.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
@@ -192,6 +195,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/WFKitLogin/WFKitLogin.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WFKitMain/WFKitMain.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WFShopMall/WFShopMall.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/WFStatistics/WFStatistics.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WKFunComponents/WKFunComponents.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WYAutoLayout/WYAutoLayout.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/dsBridge/dsBridge.framework"
